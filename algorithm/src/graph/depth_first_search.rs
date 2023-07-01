cf92eb2d20857de15bf3b970e490a6ff7fc96e4f
hungbuiknow <hungbuiknow@gmail.com>
2023-07-01 21:55:29 +0700

    Finish depth_first_search

--- algorithm/src/graph/depth_first_search.rs
+++ algorithm/src/graph/depth_first_search.rs
@@ -1,12 +1,37 @@
-use std::collections::VecDeque;
+use std::collections::{VecDeque, HashSet};
 
 pub fn depth_first_search(graph: &Graph, root: Vertex, objective: Vertex) -> Option<Vec<u32>> {
+    let mut visited: HashSet<Vertex> = HashSet::new();
     let mut history: Vec<u32> = Vec::new();
+    let mut queue = VecDeque::new();
+    queue.push_back(root);
+
+    // Get the neighbors of the parent node
+    while let Some(current_vertex) = queue.pop_front() {
+        history.push(current_vertex.value());
+
+        if current_vertex == objective {
+            return Some(history);
+        }
+
+        for neighbor in current_vertex.neighbors(graph).into_iter().rev() {
+            if visited.insert(neighbor) {
+                queue.push_front(neighbor); 
+            }
+
+        }
+    }
     None
 }
 
 #[derive(Copy, Clone, PartialEq, Eq, Hash)]
 pub struct Vertex(u32);
+
+impl core::fmt::Debug for Vertex {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_tuple("").field(&self.0).finish()
+    }
+}
 
 #[derive(Copy, Clone, PartialEq, Eq, Hash)]
 pub struct Edge(u32, u32);
@@ -73,6 +98,37 @@ mod tests {
 
         assert_eq!(
             depth_first_search(&graph, root.into(), success_objective.into()),
+            Some(correct_path)
+        );
+    }
+
+    #[test]
+    fn find_2_sucess() {
+        let vertices = vec![0, 1, 2, 3, 4, 5, 6, 7];
+        let edges = vec![
+            (0, 1),
+            (1, 3),
+            (3, 2),
+            (2, 1),
+            (3, 4),
+            (4, 5),
+            (5, 7),
+            (7, 6),
+            (6, 4),
+        ];
+
+        let root = 0;
+        let objective = 6;
+
+        let correct_path = vec![0, 1, 3, 2, 4, 5, 7, 6];
+
+        let graph = Graph::new(
+            vertices.into_iter().map(|v| v.into()).collect(),
+            edges.into_iter().map(|e| e.into()).collect(),
+        );
+
+        assert_eq!(
+            depth_first_search(&graph, root.into(), objective.into()),
             Some(correct_path)
         );
     }
