15c93e695f7821f129af9bb4eff532b7e6d6c4c6
hungknow <136663651+hungknow@users.noreply.github.com>
2023-07-02 14:23:49 +0700

    Dijkstra_breath_first_search (#6)

--- algorithm/src/graph/mod.rs
+++ algorithm/src/graph/mod.rs
@@ -4,4 +4,4 @@ mod dijkstra;
 
 pub use self::breadth_first_search::breadth_first_search;
 pub use self::depth_first_search::depth_first_search;
-pub use self::dijkstra::dijkstra;
+pub use self::dijkstra::dijkstra_breath_first_search;
