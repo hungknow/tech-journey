cba90def5aa6b7ee67d8acf63d4de1990f04c51f
hungbuiknow <hungbuiknow@gmail.com>
2023-07-02 07:22:09 +0700

    Add dijkstra

--- algorithm/src/graph/mod.rs
+++ algorithm/src/graph/mod.rs
@@ -1,5 +1,7 @@
-mod depth_first_search;
 mod breadth_first_search;
+mod depth_first_search;
+mod dijkstra;
 
-pub use self::depth_first_search::depth_first_search;
 pub use self::breadth_first_search::breadth_first_search;
+pub use self::depth_first_search::depth_first_search;
+pub use self::dijkstra::dijkstra;
