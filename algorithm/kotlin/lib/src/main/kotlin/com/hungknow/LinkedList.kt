ff74921a1f61ac24feeaf13c5482079703e4b516
hungknow <hungbuiknow@gmail.com>
2024-02-12 14:39:02 +0700

    Add LinkedList java

--- algorithm/kotlin/lib/src/main/kotlin/com/hungknow/LinkedList.kt
+++ algorithm/kotlin/lib/src/main/kotlin/com/hungknow/LinkedList.kt
@@ -1,1 +1,45 @@
-package com.hungknow.kotlin
+class LinkedList<T> {
+    class Node<T>(var data: T, var next: Node<T>? = null) {}
+
+    private var head: Node<T>? = null
+
+    fun add(data: T) {
+        val newNode = Node(data)
+        if (head == null) {
+            head = newNode
+        } else {
+            var current = head
+            while (current?.next != null) {
+                current = current.next
+            }
+            current?.next = newNode
+        }
+    }
+
+    fun addAtIndex(index: Int, data: T) {
+        val newNode = Node(data)
+        if (index == 0) {
+            newNode.next = head
+            head = newNode
+        } else {
+            var current = head
+            var count = 0
+            while (current != null && count < index - 1) {
+                current = current.next
+                count++
+            }
+            if (current != null) {
+                newNode.next = current.next
+                current.next = newNode
+            }
+        }
+    }
+
+    fun display() {
+        var current = head
+        while (current != null) {
+            println(current.data)
+            current = current.next
+        }
+    }
+}
