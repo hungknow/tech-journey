e97ef04d4afbb774303446055e33823297d8be1d
hungknow <hungbuiknow@gmail.com>
2024-02-28 15:48:31 +0700

    Update longest_consecutive

--- algorithm/python/src/longest_consecutive.py
+++ algorithm/python/src/longest_consecutive.py
@@ -7,9 +7,9 @@ def longestConsecutive(nums: List[int]) -> int:
     numsSet = set(nums)
     longest = 0
     for num in nums:
-        if num - 1 not in nums:
+        if num - 1 not in numsSet:
             length = 1
-            while (num + length) in nums:
+            while (num + length) in numsSet:
                 length += 1
             longest = max(longest, length)
 
