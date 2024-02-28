a20ac5f02b09c7bbc10797388615595fbfcd478a
hungknow <hungbuiknow@gmail.com>
2024-02-28 16:35:57 +0700

    two_sum 2 pointers

--- algorithm/python/src/twoSum.py
+++ algorithm/python/src/twoSum.py
@@ -15,7 +15,22 @@ def two_sum(nums: List[int], target: int) -> List[int]:
         dict[nums[i]] = i
     # If no solution is found, return an empty list
     return []
-
+    
+# Use two pointers
+def two_sum_2(nums: List[int], target: int) -> List[int]:
+    l = 0
+    r = len(nums) - 1
+    while l < r:
+        sum = nums[l] + nums[r]
+        if sum == target:
+            return [l, r]
+        
+        if sum > target:
+            r -= 1
+        if sum < target:
+            l += 1
+    
+    return []
 # Test cases
 class TwoSumTests(unittest.TestCase):
     def test_example_case_1(self):
@@ -23,50 +38,25 @@ class TwoSumTests(unittest.TestCase):
         target = 9
         expected = [0, 1]
         self.assertEqual(two_sum(nums, target), expected)
+        self.assertEqual(two_sum_2(nums, target), expected)
 
     def test_example_case_2(self):
-        nums = [3, 2, 4]
+        nums = [2, 3, 4]
         target = 6
-        expected = [1, 2]
+        expected = [0, 2]
         self.assertEqual(two_sum(nums, target), expected)
+        self.assertEqual(two_sum_2(nums, target), expected)
 
     def test_example_case_3(self):
         nums = [3, 3]
         target = 6
         expected = [0, 1]
         self.assertEqual(two_sum(nums, target), expected)
+        self.assertEqual(two_sum_2(nums, target), expected)
 
     def test_no_solution(self):
         nums = [1, 2, 3, 4]
         target = 10
         expected = []
         self.assertEqual(two_sum(nums, target), expected)
-    def setUp(self):
-        self.two_sum = two_sum
-
-    def test_example_case_1(self):
-        nums = [2, 7, 11, 15]
-        target = 9
-        expected = [0, 1]
-        self.assertEqual(self.two_sum(nums, target), expected)
-
-    def test_example_case_2(self):
-        nums = [3, 2, 4]
-        target = 6
-        expected = [1, 2]
-        self.assertEqual(self.two_sum(nums, target), expected)
-
-    def test_example_case_3(self):
-        nums = [3, 3]
-        target = 6
-        expected = [0, 1]
-        self.assertEqual(self.two_sum(nums, target), expected)
-
-    def test_no_solution(self):
-        nums = [1, 2, 3, 4]
-        target = 10
-        expected = []
-        self.assertEqual(self.two_sum(nums, target), expected)
-
-if __name__ == '__main__':
-    unittest.main()
+        self.assertEqual(two_sum_2(nums, target), expected)
