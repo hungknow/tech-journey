fdcda99282db0eafbf0e10e2a115c49225127cf6
hungknow <hungbuiknow@gmail.com>
2024-02-29 10:06:24 +0700

    Update src/token_bucket_test.py

--- algorithm/python/src/token_bucket_test.py
+++ algorithm/python/src/token_bucket_test.py
@@ -4,7 +4,7 @@ import unittest
 from src.token_bucket import TokenBucket
 
 class TokenBucketTest(unittest.TestCase):
-    def test(self):
+    def test1(self):
         throttle = TokenBucket(1, 1)
         self.assertEqual(throttle.consume(1), 0)
         self.assertEqual(throttle.consume(1), 1)
@@ -18,3 +18,20 @@ class TokenBucketTest(unittest.TestCase):
         self.assertEqual(throttle.consume(1), 0)
         time.sleep(2)
         self.assertEqual(throttle.consume(2), 1)
+
+    def test2(self):
+        throttle = TokenBucket(2, 1)
+        self.assertEqual(throttle.consume(1), 0)
+        self.assertEqual(throttle.consume(1), 0)
+        self.assertEqual(throttle.consume(1), 1)
+        self.assertEqual(throttle.consume(1), 1)
+        time.sleep(1)
+        self.assertEqual(throttle.consume(2), 0)
+        time.sleep(0.45)
+        self.assertEqual(throttle.consume(1), 1)
+        time.sleep(0.05)
+        self.assertEqual(throttle.consume(1), 0)
+        time.sleep(0.5)
+        self.assertEqual(throttle.consume(1), 0)
+        time.sleep(1)
+        self.assertEqual(throttle.consume(2), 0)
